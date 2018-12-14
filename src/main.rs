c01baf7e8a1a140978b5ccc2ddd7111e94d33957
Colin Merkel <colinmerkel@google.com>
2018-12-13 17:21:12 -0800

    usability improvements

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,11 @@ use std::io::{self, Read};
 fn main() {
     let mut buffer = String::new();
     io::stdin().read_to_string(&mut buffer).unwrap();
-    let selections = buffer.split("\n").collect::<Vec<_>>();
+    let selections = buffer.split("\n").map(|s| s.trim()).filter(|s| !s.is_empty()).collect::<Vec<_>>();
+
+    if selections.len() == 0 {
+        return
+    }
 
     let selection = Select::with_theme(&ColorfulTheme::default())
         .default(0)
